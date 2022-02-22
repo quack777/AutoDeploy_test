77920416c5e912841be2f3523ae940fc4df26beb
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-23 04:39:59 +0900

    Add: 배포 전 마지막 세팅

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
 
+app.use(express.static('build'));
+
 app.get('/', function (req, res) {
-  res.send('hello!sdfasdfsa');
+  res.sendFile(__dirname + '/build/index.html');
 });
 
 app.listen(3000, () => {
